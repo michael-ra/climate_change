c42fc0d6080a4fdc78248a1497edd5aef175a4eb
Paula Harder <paulaharder@posteo.de>
2021-01-16 23:32:09 +0100

    first prediction

--- remove_sea_tiles.py
+++ remove_sea_tiles.py
@@ -3,13 +3,17 @@ import numpy as np
 
 counts_dat = pd.read_csv('./data/fire_precip_temp.csv')
 counts_array = counts_dat.to_numpy()
-
+counts_dat.sort_values('year')
+counts_dat.sort_values('month')
+year_vals=counts_dat['year'][:].to_numpy()
 burned_cells = counts_dat['BurnedCells'][:]
 land_cells = counts_dat['LandCells'][:]
 precip_std = counts_dat['precip_std'][:].to_numpy()
 precip_mean = counts_dat['precip_mean'][:].to_numpy()
 temp_std = counts_dat['temp_std'][:].to_numpy()
 temp_mean = counts_dat['temp_mean'][:].to_numpy()
+v = counts_dat['v'][:].to_numpy()
+h = counts_dat['h'][:].to_numpy()
 
 burned_cells_array = burned_cells.to_numpy()
 land_cells_array = land_cells.to_numpy()
@@ -21,15 +25,48 @@ precip_stds =[]
 temp_means = []
 temp_stds =[]
 land_cells = []
-for i in range(shape[0]):
+v_values = []
+h_values = []
+t_mean_past_month = []
+t_std_past_month = []
+p_mean_past_month = []
+p_std_past_month = []
+
+years=[]
+
+for i in range(1,shape[0]):
     if land_cells_array[i] > 10: 
-        if not (np.isnan(burned_cells_array[i]) or np.isnan(precip_std[i]) or np.isnan(precip_mean[i]) or np.isnan(temp_std[i]) or np.isnan(temp_mean[i])):
+        if not (np.isnan(burned_cells_array[i]) or np.isnan(precip_std[i]) or np.isnan(precip_mean[i]) or np.isnan(temp_std[i]) or np.isnan(temp_mean[i]) or np.isnan(precip_std[i-1]) or np.isnan(precip_mean[i-1]) or np.isnan(temp_std[i-1]) or np.isnan(temp_mean[i-1])):
             burned_cleaned.append(burned_cells_array[i])
             precip_stds.append(precip_std[i])
             precip_means.append(precip_mean[i])
             temp_stds.append(temp_std[i])
             temp_means.append(temp_mean[i])
             land_cells.append(land_cells_array[i])
+            v_values.append(v[i])
+            h_values.append(h[i])
+            t_mean_past_month.append(temp_mean[i-1])
+            t_std_past_month.append(temp_std[i-1])
+            p_mean_past_month.append(precip_mean[i-1])
+            p_std_past_month.append(precip_mean[i-1])
+            years.append(year_vals[i])
+            
+ltc = np.zeros((17,len(v_values)))
+ltc_dat = pd.read_csv('./data/LCT_counts.csv')
+ltc_year = ltc_dat['year'][:].to_numpy()
+ltc_v = ltc_dat['v'][:].to_numpy()
+ltc_h= ltc_dat['h'][:].to_numpy()
+n_pixels = ltc_dat['n_pixels'][:].to_numpy()
+ltcs = ltc_dat['lct'][:].to_numpy()
+ltc_count= len(n_pixels)
+for i in range(len(v_values)):
+    for j in range(ltc_count):
+        if (ltc_year[j]==years[i] and ltc_v[j]==v_values[i] and ltc_h[j] == h_values[i]):
+            for k in range(16):
+                if ltcs[j]== k+1:
+                    ltc[k,i]=n_pixels[j]
+            
+    
 
 np.save('./data/fire_counts',np.array(burned_cleaned))
 np.save('./data/precip_mean',np.array(precip_means))
@@ -37,3 +74,10 @@ np.save('./data/precip_std',np.array(precip_stds))
 np.save('./data/temp_mean',np.array(temp_means))
 np.save('./data/temp_std',np.array(temp_stds))
 np.save('./data/land_cells',np.array(land_cells))
+np.save('./data/v_coord',np.array(v_values))
+np.save('./data/h_coord',np.array(h_values))
+np.save('./data/t_mean_past_month',np.array(t_mean_past_month))
+np.save('./data/t_std_past_month',np.array(t_std_past_month))
+np.save('./data/p_mean_past_month',np.array(p_mean_past_month))
+np.save('./data/p_std_past_month',np.array(p_std_past_month))
+np.save('./data/ltc',ltc)
--- simple_regressors.py
+++ simple_regressors.py
@@ -1,11 +1,13 @@
 print('Loading modules...')
 import pickle
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.preprocessing import MinMaxScaler, Normalizer
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 import sklearn
 import argparse
+import numpy as np
 
 #processing the arguments
 parser = argparse.ArgumentParser()
@@ -13,31 +15,53 @@ parser.add_argument("--regressor", default='rf', help="choose a regressor, eithe
 args = parser.parse_args()
 regressor = args.regressor
 
-input_dim = 4 #maybe more later
+input_dim = 21 #maybe more later
 
 #load data
 print('Loading data...')
 
 
-precipitation = np.load('./data/era5_precipitaion_mean_var.npy') #assuming dim n_pointsx2
-temperature = np.load('./data/era5_temperature_mean_var.npy') #assuming dim n_pointsx2
+precip_mean = np.load('./data/precip_mean.npy')
+precip_std = np.load('./data/precip_std.npy')
+temp_mean = np.load('./data/temp_mean.npy')
+temp_std = np.load('./data/temp_std.npy')
+land_cells = np.load('./data/land_cells.npy')
+v_coord = np.load('./data/v_coord.npy')
+h_coord = np.load('./data/h_coord.npy')
+p_m_past_month = np.load('./data/p_mean_past_month.npy')
+p_s_past_month = np.load('./data/p_std_past_month.npy')
+t_m_past_month = np.load('./data/t_mean_past_month.npy')
+t_s_past_month = np.load('./data/t_std_past_month.npy')
+ltc = np.load('./data/ltc.npy')
 
-fire_counts = np.load(path_to_fire_counts) #assuming dim n_pointsx1
+fire_counts = np.load('./data/fire_counts.npy') #assuming dim n_pointsx1
 n_points = len(fire_counts)
 
 X = np.zeros((n_points, input_dim))
 y = fire_counts
 
 for i in range(n_points):
-    X[i,0] = precipitation[i,0]
-    X[i,1] = precipitation[i,1]
-    X[i,2] = temperature[i,0]
-    X[i,3] = temperature[i,1]
-
+    X[i,0] = precip_mean[i]
+    X[i,1] = precip_std[i]
+    X[i,2] = temp_mean[i]
+    X[i,3] = temp_std[i]
+    X[i,4] = land_cells[i]
+    for j in range(16):
+        X[i,5+j]=ltc[j,i]
+    #X[i,21] = v_coord[i]
+    #X[i,22] = h_coord[i]
+    #X[i,5] = p_m_past_month[i]
+    #X[i,6] = p_s_past_month[i]
+    #X[i,5] = t_s_past_month[i]
+    #X[i,8] = t_s_past_month[i]
+    
 
 # X will have dim. #data points x input_dim, y #data_points
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=4/14, random_state=42, shuffle=False)
+print(n_points)
+#scaler = MinMaxScaler().fit(y)
+#y = scaler.transform(y.reshape(1,-1))
+#y = np.interp(y, (y.min(), y.max()), (0, 1))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=5/20, random_state=42, shuffle=False)
 
 if regressor == 'rf':
     regr = RandomForestRegressor()
@@ -47,15 +71,18 @@ elif regressor == 'gbt':
     regr = GradientBoostingRegressor()
 
 print('Training Regressor...')
+scaler = MinMaxScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 regr.fit(X_train,y_train)
 
 #save regressor
 print('Saving regressor...')
-filename = './models/'+regressor'.sav'
+filename = './data/models/'+regressor+'.sav'
 pickle.dump(regr, open(filename, 'wb'))
 #save regressor features
-if regressor == 'rf':
-    np.save('/models/regressor_feats/feat_imp_'+regressor,regr.feature_importances_)
+#if regressor == 'rf':
+ #   np.save('./data/models/regressor_feats/feat_imp_'+regressor,regr.feature_importances_)
 
 #make prediction 
 print('Predicting on test set...')
